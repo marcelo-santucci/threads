cbb402d23c27849f7002d6ff26d4e1353152bdb8
marcelo-santucci <marcelo.santucci@gmail.com>
2026-03-01 15:47:18 -0600

    Update doslimitexecthread.c

--- doslimitexecthread.c
+++ doslimitexecthread.c
@@ -1,3 +1,11 @@
+/*
+* En este programa se crea un thread para ejecutar el procedimiento miTurno
+* y luego se llama al procedimiento tuTurno pero la diferencia es que en este
+* código luego se hace que la ejecución de ese procedimiento se una al thread
+* que se creó para el primero.
+* 
+* El resultado es ahora si se ejecutan ambos procedimientos. 
+*/
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
